6584821eaf27ff843b6e9f5a85ba5256bc7f272a
Jetttay <64567613+Jetttay1980@users.noreply.github.com>
2024-09-07 02:10:40 +1000

    Update app.js

--- E-Commerce-Website-Using-NodeJS-master/app.js
+++ E-Commerce-Website-Using-NodeJS-master/app.js
@@ -16,7 +16,7 @@ const cartRoutes = require('./routes/cart/cart');
 
 
 
-mongoose.connect('mongodb://admin:pass@localhost:27017/admin', {
+mongoose.connect('mongodb://mongo:27017/shopping-cart', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(async () => {
